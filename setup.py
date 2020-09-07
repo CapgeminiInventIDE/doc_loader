27a562850c93d7f125cb8f7b51bca1ad8fa0dbd5
Andy Challis <andy.challis@capgemini.com>
2020-09-08 02:51:00 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="doc_loader",
-    version="0.0.1",
+    version="0.1.0",
     author="Capgemini Invent IDE",
     description="Given werkzeug.FileStorage, fastapi.UploadFile or str file path as input it converts any image files(.pdf, .jpg, .png, .tiff) into list of PIL or numpy objects",
     long_description=long_description,
